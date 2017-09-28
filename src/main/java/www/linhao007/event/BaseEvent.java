9ad1ec7aaaa03e3facec379dc5855c24e6aba20b
linhu <linhu@meituan.com>
2017-09-28 19:56:47 +0800

    update

--- src/main/java/www/linhao007/event/BaseEvent.java
+++ src/main/java/www/linhao007/event/BaseEvent.java
@@ -4,5 +4,6 @@ package www.linhao007.event;
  * Created by linhu on 17/9/28.
  */
 public abstract class BaseEvent<T> {
+    private static final long serialVersionUID = 1000L;
     public abstract T getDto();
 }
